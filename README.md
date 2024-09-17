9461c6f3d53e38b27e934e250a65072be4457eb7
Luis Mora <ljmora004@outlook.com>
2024-09-17 18:36:16 -0500

    Mis Hooks y Helpes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repositorio con Custom Hooks y Helpers para React
+
+Aquí se almacenan todos los archivos reutilizables para mis proyectos de REACT como lo son los Custom Hooks y Helpers que pueda necesitar.
---
+++ useTodo.js
@@ -0,0 +1,56 @@
+
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
+const initialState = JSON.parse(localStorage.getItem('todos') || []);
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+    }, [todos]);
+
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        };
+
+        dispatch(action);
+    };
+
+    const handleDeleteTodo = (todo) => {
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: todo.id
+        };
+
+        dispatch(action);
+    }
+
+    const handleToggleTodo = (todo) => {
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: todo.id
+        };
+
+        dispatch(action);
+    }
+
+    const todosCounter = todos.length;
+    const todosPendCounter = todos.filter(todo => !todo.done).length;
+
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCounter,
+        todosPendCounter
+    }
+};
